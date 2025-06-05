42e5545466be8c62c8dd80ae798d292f82bddf97
Balder <bafl@hopvgskole.no>
2025-06-05 09:04:42 +0200

    Registrerings skjema ferdig

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const sqlite3 = require("sqlite3").verbose();
-const session = require("express-session");
-const bcrypt = require("bcrypt");
+
 
 const app = express();
 const port = 3000;
@@ -17,25 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /** Konfigurerer sesjonshåndtering */
-app.use(
-    session({
-        secret: "hemmeligNøkkel",
-        resave: false,
-        saveUninitialized: true,
-        cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
-    })
-);
 
-function isAuthenticated(req, res, next) {
-    if (req.session.user) {
-        next();
-    } else {
-        res.redirect("/login");
-    }
-}
+
+
 
 /** Kobler til SQLite-database */
-const db = new sqlite3.Database("minNyeDatabase.db", (err) => {
+const db = new sqlite3.Database("eksamen.db", (err) => {
     if (err) {
         console.error("Feil ved tilkobling til database:", err.message);
     } else {
@@ -49,26 +35,11 @@ app.get("/", (req, res) => {
 });
 
 /** Rute: Viser innloggingssiden */
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "view", "login.html"));
+app.get("/registrer", (req, res) => {
+    res.sendFile(path.join(__dirname, "view", "registrer.html"));
 });
 
-/** Rute: Viser siden for å opprette ny bruker */
-app.get("/ny-bruker", (req, res) => {
-    res.sendFile(path.join(__dirname, "view", "ny-bruker.html"));
-});
 
-/** Rute: Viser privat side (kun for autentiserte brukere) */
-app.get("/privat", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "view", "privat.html"));
-});
-
-/** Rute: Logger ut brukeren og avslutter sesjonen */
-app.get("/logout", (req, res) => {
-    req.session.destroy();
-    res.clearCookie("connect.sid");
-    res.redirect("/login");
-});
 
 /**
  * Rute: Håndterer innlogging
